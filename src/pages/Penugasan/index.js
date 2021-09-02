c8d040ada600c2a5d768f391bb5b5d9ecd0265e2
labiebhn <labiebhn@gmail.com>
2021-09-02 17:53:44 +0700

    Adjust for screen penugasan

--- src/pages/Penugasan/index.js
+++ src/pages/Penugasan/index.js
@@ -10,6 +10,7 @@ import {
   IcSinkron,
 } from '../../assets';
 import {Gap, HeaderDetail, Select} from '../../components';
+import { setPenugasanValue } from '../../utils';
 import storage from '../../utils/storage';
 
 const Penugasan = ({navigation}) => {
@@ -34,25 +35,6 @@ const Penugasan = ({navigation}) => {
         console.error(err.response);
       });
   }, []);
-
-  const setPenugasanValue = (value) => {
-    switch(value) {
-      case 'LMO':
-        return 'Area Tambang LMO';
-      case 'BMO I':
-        return 'Area Tambang BMO I';
-      case 'BMO II':
-        return 'Area Tambang BMO II';
-      case 'SMO':
-        return 'Area Tambang SMO';
-      case 'BEE':
-        return 'Area Tambang BEE';
-      case 'GMO':
-        return 'Area Tambang GMO';
-      default:
-        return 'Area Tambang LMO';
-    }
-  }
 
   return (
     <View style={styles.page}>
